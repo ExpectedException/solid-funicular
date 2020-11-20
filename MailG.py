fd2428fd7bb5ef00206c1fa21295a569ba1514c3
ExpectedException <6000mmr@mail.ru>
2020-11-20 12:45:22 +0300

    Update MailG.py

--- MailG.py
+++ MailG.py
@@ -11,12 +11,20 @@ import json
 from nickname_generator import generate
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from threading import Thread
 
 
 def passgen(x):
     characters = string.ascii_letters + string.digits
     password = "".join(choice(characters) for x in range(randint(14, 16)))
     return password
+
+
+def sav(Email, Passwd):
+    f = open("data_done100percent.txt", "a")
+    data = str(Email + ' ' + Passwd + '' + '\n')
+    f.write(data)
+    f.close()
 
 
 def retoggleAllTheAddons(driver):
@@ -33,14 +41,16 @@ def retoggleAllTheAddons(driver):
     """)
 
 
-accname =(generate() + generate() + str(randint(100, 9000)))
-Email =(accname + "@mail.ru")
-Passwd= passgen(randint(1, 200))
+def degenerate():
+    accname =(generate() + generate() + str(randint(100, 9000)))
+    Email =(accname + "@mail.ru")
+    Passwd= passgen(randint(1, 23243400))
+    return accname, Email, Passwd
 
 
-def main():
+def main(accname, Email, Passwd):
     f = open("data.txt", "a")
-    data = str(Email + ':' + Passwd + ':' + '\n')
+    data = str(Email + ' ' + Passwd + '' + '\n')
     f.write(data)
     f.close()
     print(Email)
@@ -68,18 +78,27 @@ def main():
     wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#password'))).send_keys(Passwd)
     wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#repeatPassword'))).send_keys(Passwd)
     #wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#phone-number__phone-input'))).send_keys('9789601886') #1
-    wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#phone-number__phone-input'))).send_keys('9787393986') #2
+    #wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#phone-number__phone-input'))).send_keys('9787393986') #2
     time.sleep(0.5)
     wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[3]/div[3]/div/div/div/form/button'))).click()
     wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '.c2182'))).click()
-    driver.get('https://id.mail.ru/contacts')
-    wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, 'button.base-0-2-77:nth-child(1) > div:nth-child(1) > svg:nth-child(1)'))).click()
-    wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/div/div/div[2]/form/div[4]/button[1]/span'))).click()
-    wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/div/div/div[2]/div/button/span'))).click()
-    f = open("data_done100percent.txt", "a")
-    data = str(Email + ':' + Passwd + ':' + '\n')
-    f.write(data)
-    f.close()
+    sav(Email, Passwd)
+    #driver.get('https://id.mail.ru/contacts')
+    #wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, 'button.base-0-2-77:nth-child(1) > div:nth-child(1) > svg:nth-child(1)'))).click()
+    #wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/div/div/div[2]/form/div[4]/button[1]/span'))).click()
+    #wait.until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/div/div/div[2]/div/button/span'))).click()
     driver.close()
 
-main()
+
+if __name__ == '__main__':
+    threads = []
+    for n in range(7):
+        time.sleep(0.1)
+        t = Thread(target=main, args=(degenerate()))
+        time.sleep(0.1)
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+
+
